73885164081c36e754e8389b73673ec79c431a24
John Hooks <johnhookes@aol.com>
2024-11-06 17:14:23 +0000

    Created base.html as the main layout template and added the homepage view and template structure.

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from django.views import generic
 from .models import Post
 
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.all()
-    template_name = "post_list.html"
+    queryset = Post.objects.filter(status=1)
+    template_name = "blog/index.html"
+    paginate_by = 6
     
